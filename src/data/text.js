226624ab009835b949886297f7278d12b13633ed
c1arl5o <carlobockermann@gmail.com>
2026-01-03 13:42:21 +0100

    implement randomized texts too

---
+++ src/data/text.js
@@ -0,0 +1,27 @@
+export const feedTexts = [
+  (name, monster) => (
+    <>
+      <strong>{name}</strong> hat sich gerade einen <strong>{monster}</strong> genehmigt.
+    </>
+  ),
+  (name, monster) => (
+    <>
+      <strong>{name}</strong> kann dank einem <strong>{monster}</strong> das Leben geniessen.
+    </>
+  ),
+  (name, monster) => (
+    <>
+      <strong>{name}</strong> braucht jetzt einen <strong>{monster}</strong>.
+    </>
+  ),
+  (name, monster) => (
+    <>
+      <strong>{name}</strong> trinkt gerade einen schönen <strong>{monster}</strong>.
+    </>
+  ),
+  (name, monster) => (
+    <>
+      <strong>{name}</strong> macht sich gerade einen eiskalten <strong>{monster}</strong> auf.
+    </>
+  ),
+];
